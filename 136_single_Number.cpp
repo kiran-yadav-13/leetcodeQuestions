8752c38af3abcb6129eada737fab99e863658f96
Kiran Yadav <kiranyadav9551@gmail.com>
2025-12-08 11:40:05 +0530

    Add alternative approach to find single number
    Implemented a second approach using a map to find the single number.

--- 136_single_Number.cpp
+++ 136_single_Number.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        //Approach 1
         // in this solution we know that the xor of same number is 0 and the 0^num=num
         int num=0;
-        for(int i=0; i<nums.size(); i++){
-            num=nums[i]^num;
+        // for(int i=0; i<nums.size(); i++){
+        //     num=nums[i]^num;
+        // }
+
+        //Approach 2
+    map<int,int> m;
+    for(int i=0; i<nums.size(); i++){
+        if(m.count(nums[i])>0){
+        m[nums[i]]++;
+        }else{
+            m.insert({nums[i],1});
         }
+    }
+    for(auto it:m){
+        if(it.second==1){
+            num=it.first;
+        }
+    }
         return num;
     }
 };
