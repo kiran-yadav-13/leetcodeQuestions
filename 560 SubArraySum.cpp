6a255a9f1868db2d93c9531e52aea09e7b03fe0b
Kiran Yadav <kiranyadav9551@gmail.com>
2025-12-09 11:02:23 +0530

    adding another approach of  subarraySum implementation
    Refactor subarraySum function to improve clarity and efficiency.

--- 560 SubArraySum.cpp
+++ 560 SubArraySum.cpp
@@ -11,25 +11,34 @@ public:
     //     }
     //     return count;
     // }
-    int subarraySum(vector<int>& nums, int k) {
-
-        unordered_map<int, int> prefixsum;
-        prefixsum[0]=1; // Base case: a Subarray sum starting at index 0
-
-        int sum=0, count=0; 
-        for(int num: nums){
-            sum+=num; // compute running sum
+      int subarraySum(vector<int>& nums, int k) {
+        // in the array (..l-1,l..r)that has currentsum =sum
+        //some elemnts add upto give the sum k (l....r)
+        // some elemnts before this sum adds upto give sum-k  (....l-1)
         
-
-        if(prefixsum.find(sum-k) !=prefixsum.end()){
-            count+=prefixsum[sum-k]; // add all occurences of (sum-k)
+       // i = 1 (element = 2)
+// sum = prefix[1] = 3
+// need = sum - k = 3 - 3 = 0
+// Do we have any earlier prefix with value 0? Yes — at index -1 (the empty prefix).
+// Each earlier prefix index j where prefix[j] == need gives a subarray j+1 .. i
+// Here j = -1 → subarray (-1 + 1) .. 1 = 0 .. 1 → [1, 2]
+// So we found one subarray: indices 0 to 1.
+//unordered_map- due to its lookup time is O(1)
+       unordered_map<int, int>freq; //store the count of prefix sum
+       int count=0;
+       int sum=0; // 
+       freq[0]=1;// we are taking 
+       for(int i=0; i<nums.size(); i++){
+        sum+=nums[i];
+        int rem=sum-k;
+        if(freq.find(rem)!=freq.end()){
+            count+=freq[rem];
         }
-
-        prefixsum[sum]++;
-        }
-          return count;
+        freq[sum]++;
+       }
+       return count;
     }
-  
+
 
 
 
