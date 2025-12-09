ae4f6231c2ae64541df412a0ac6051b67f72b2cc
Kiran Yadav <kiranyadav9551@gmail.com>
2025-12-09 15:38:50 +0530

    adding multiple approaches for twoSum implementation
    Refactor twoSum function to use sorting and two-pointer technique.

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,17 +1,50 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    
-        vector<int>ans={-1,-1};
+        vector<int>ans;
+        //Approach1 
+        // for(int i=0; i<nums.size(); i++ ){
+        //     for(int j=i+1; j<nums.size(); j++){
+        //         if(nums[i]+nums[j]==target){
+        //             ans.push_back(i);
+        //              ans.push_back(j);
+        //              return ans;
+        //         }
+        //     }
+        // }
+
+        //Approach2
+        // map<int, int> m;
+        // for(int i=0; i<nums.size(); i++){
+        //     if(m.find(target-nums[i])!=m.end()){
+        //         return {i,m[target-nums[i]]};
+        //     }
+        //     m[nums[i]]=i;
+        // }
+
+        //Approach 3
+   
+        vector<pair<int,int>> v;
         for(int i=0; i<nums.size(); i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if((nums[i]+nums[j])==target){
-                    ans[0]=i;
-                    ans[1]=j;
-                }
-            }
+            v.push_back({nums[i],i});
         }
-        
-        return ans;
+         int i=0;
+        int j=v.size()-1; 
+        sort(v.begin(), v.end());
+        int sum=0; 
+        while(i<j){
+            sum=v[i].first+v[j].first;
+            if(sum==target){
+                return {v[i].second,v[j].second};
+            }
+            else if(sum>target){
+                j--;
+            }
+            else if (sum<target){
+                i++;
+            }
+           
+        }
+        return {-1,-1};
     }
 };
